120c0d6b348c8fe727dab6ff46d5acfa7beb5f24
James <james.zarsuelo@indigo21.com>
2024-09-04 21:31:15 +0800

    modify the app routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import RootLayout from "../layout/RootLayout";
-// import Home from "../pages/Home/Home";
+import Home from "../pages/Home/Home";
 import MapCustom from "../pages/Map/MapCustom";
-import MapCustom2 from "../pages/Map/MapCustom-backup-new";
+// import MapCustom2 from "../pages/Map/MapCustom-backup-new";
 
 const AppRoutes = [
     {
@@ -11,7 +11,7 @@ const AppRoutes = [
             {index: true, element: <MapCustom />},
             // {index: true, element: <Home />},
             {path: '/map', element: <MapCustom />},
-            {path: '/map2', element: <MapCustom2 />},
+            {path: '/map2', element: <Home />},
         ]
     }
 ];
