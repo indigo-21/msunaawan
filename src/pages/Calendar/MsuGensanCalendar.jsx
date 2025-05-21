90639ffc00e71fdd1ce36bcf0959c83651b28ded
cyrusjameszarsuelo <cyrusjames.cjz@gmail.com>
2025-05-21 20:31:55 +0800

    changes in css

--- src/pages/Calendar/MsuGensanCalendar.jsx
+++ src/pages/Calendar/MsuGensanCalendar.jsx
@@ -57,7 +57,7 @@ function MsuGensanCalendar() {
     }
 
     return (
-        <div>
+        <div className="bg-transparent w-screen h-screen flex flex-col items-center justify-center">
             <Calendar
                 eventData={eventDataFormat}
                 colorScheme={COLORSCHEME}
